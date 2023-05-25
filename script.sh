2b6eee87e99428e27f0e4c11a11812402a5afc29
ashu3103 <ashu3103@ROS.myguest.virtualbox.org>
2023-05-25 16:29:02 +0530

    Txt files

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+paths=('movement_of_nodes' 'no_of_nodes' 'traffic')
+iters=('1' '2' '3')
+
+for i in "${paths[@]}"
+do
+    for entry in `ls $i`;do
+        filepath="${i}/${entry}"
+        readarray -d . -t filename <<< "$entry"
+        echo > "${i}/${filename}.txt"
+        ns $filepath
+        gawk -f delay.awk "${filename}.tr" > "${i}/${filename}.txt"
+    done
+done;
+
+#gawk -f delay.awk traffic/15_random_tcp.tr
